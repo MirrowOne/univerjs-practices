53e7a24bf599c02c5fabe2c96bd361c963f3540c
Jonas <jjalvarezmendoza20@gmail.com>
2025-12-03 13:34:00 -0400

    feat(useValidationsRules): agregar cancelacion de edicion cuando la la validacion no es correcta

--- src/features/Univer/hooks/useValidationsRules.ts
+++ src/features/Univer/hooks/useValidationsRules.ts
@@ -1,12 +1,11 @@
-import { sheetDataType } from "@/src/types/sheets-types";
-import { useFWorkbook, useFWorksheet, useUniverAPI } from "@/src/store/univerStore";
+import { useFWorksheet, useUniverAPI } from "@/src/store/univerStore";
+import { useCallback } from "react";
 
 export const useValidationsRules = () => {
     const fworksheet = useFWorksheet();
     const univerAPI = useUniverAPI();
 
-    // Create a new data validation rule that requires a number between 1 and 10 fot the range A1:B10
-    const initializeRules = () => {
+    const numberBetweenRule = useCallback(() => {
         const fRange = fworksheet.getRange('A1:B10')
         const rule = univerAPI?.newDataValidation()
             .requireNumberBetween(1, 10)
@@ -15,10 +14,16 @@ export const useValidationsRules = () => {
                 showErrorMessage: true,
                 error: 'El valor debe estar entre 1 y 10',
                 errorTitle: 'Error de validación',
+                errorStyle: univerAPI.Enum.DataValidationErrorStyle.STOP
             })
             .build();
 
         fRange.setDataValidation(rule);
+    }, [fworksheet, univerAPI]);
+
+    // Create a new data validation rule that requires a number between 1 and 10 fot the range A1:B10
+    const initializeRules = () => {
+        numberBetweenRule();
     };
 
     return {
