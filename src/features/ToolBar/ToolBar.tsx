adaea50f04bf4ef8acc8a91c120c4feb7b59f917
Jonas <jjalvarezmendoza20@gmail.com>
2025-12-04 14:40:08 -0400

    chore: comentar y eliminar el uso de las funciones utilitarias de parseToA1Notation

--- src/features/ToolBar/ToolBar.tsx
+++ src/features/ToolBar/ToolBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./ToolBar.module.css";
 import { useToolbarUtils } from "./hooks/useToolbarUtils";
-import { columnToLetter, toA1Notation } from "@/src/utils/parseToA1Notation";
+// import { columnToLetter, toA1Notation } from "@/src/utils/parseToA1Notation";
 
 const sheetMockData = [
   ["Nombre", "Apellido", "Edad"],
@@ -9,12 +9,12 @@ const sheetMockData = [
   ["Maria", "Lopez", "30"],
 ];
 
-const rangeObject = {
-  startRow: 0,
-  startColumn: 0,
-  endRow: 10,
-  endColumn: 1,
-};
+// const rangeObject = {
+//   startRow: 0,
+//   startColumn: 0,
+//   endRow: 10,
+//   endColumn: 1,
+// };
 
 export const ToolBar = () => {
   const { setValues, navigateToCell, getSheetStyles } = useToolbarUtils();
@@ -31,23 +31,21 @@ export const ToolBar = () => {
     getSheetStyles();
   };
 
-  const handleToA1Notation = () => {
-    const a1Notation = toA1Notation(rangeObject);
-    console.log(a1Notation);
-  };
+  // const handleToA1Notation = () => {
+  //   const a1Notation = toA1Notation(rangeObject);
+  //   console.log(a1Notation);
+  // };
 
-  const handleColumnToLetter = () => {
-    const columnLetter = columnToLetter(rangeObject.startColumn);
-    console.log(columnLetter);
-  };
+  // const handleColumnToLetter = () => {
+  //   const columnLetter = columnToLetter(rangeObject.startColumn);
+  //   console.log(columnLetter);
+  // };
 
   return (
     <section className={styles.toolBarContainer}>
       <button onClick={handleSetValues}>Buscar</button>
       <button onClick={handleNavigateToCell}>Navegar a celda</button>
       <button onClick={handleGetSheetStyles}>Obtener estilos</button>
-      <button onClick={handleToA1Notation}>Obtener notación A1</button>
-      <button onClick={handleColumnToLetter}>Obtener letra de columna</button>
     </section>
   );
 };
